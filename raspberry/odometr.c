b805fc5209b146a4871f2c4fbce1bdd9b7811445
ilya <ilya.blan4@gmail.com>
2014-07-03 19:19:44 +0700

    add simple odometr

---
+++ raspberry/odometr.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <wiringPi.h>
+
+int main(){
+	const int pin = 9;	
+	float wheel_length = 2133.0; // in millimeters
+	float multiplier = 3.6; // to convert from m/s to km/h
+	
+	printf("Start bike\n");
+	wiringPiSetup();
+	pullUpDnControl(pin, PUD_UP);
+	pinMode(pin, INPUT);
+	
+	float speed;
+	int last_cycle = millis();
+	
+	for(;;) {
+		while(digitalRead(pin) == HIGH) { delay(1); } // whate for cycle
+		
+		speed = wheel_length / (millis() - last_cycle); // in m/s
+		printf("%f\n", speed * multiplier);
+		last_cycle = millis();
+
+		while(digitalRead(pin) == LOW){ delay(1); } // whate for unlocking reed switch (gerkon in rus)
+	}
+
+	return 0;
+}
