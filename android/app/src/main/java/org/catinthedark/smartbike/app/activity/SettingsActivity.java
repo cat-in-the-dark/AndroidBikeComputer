2ef24d5e57559316074c74d8d17a18c10406fe69
Kirill <kolbasisha@gmail.com>
2014-07-09 17:27:53 +0700

    Removed wheel size calculator.

--- android/app/src/main/java/org/catinthedark/smartbike/app/activity/SettingsActivity.java
+++ android/app/src/main/java/org/catinthedark/smartbike/app/activity/SettingsActivity.java
@@ -5,57 +5,42 @@ import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.NumberPicker;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+
 import org.catinthedark.smartbike.app.R;
 import org.catinthedark.smartbike.app.calculator.SpeedCalculator;
 
 public class SettingsActivity extends ActionBarActivity {
-
-    private final String RIM_DIAMETER_KEY = "rim_diameter";
-    private final String TIRE_THICKNESS_INDEX_KEY = "tire_thickness";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        SharedPreferences preferences = getSharedPreferences(MainActivity.SHARED_PREFS_NAME, 0);
-
-        final NumberPicker diameterNumberPicker = (NumberPicker) findViewById(R.id.diameterNumberPicker);
-        diameterNumberPicker.setMaxValue(SpeedCalculator.RIM_DIAMETER_MAX);
-        diameterNumberPicker.setMinValue(SpeedCalculator.RIM_DIAMETER_MIN);
-        diameterNumberPicker.setValue(preferences.getInt(RIM_DIAMETER_KEY, SpeedCalculator.RIM_DIAMETER_MIN));
-
-        final NumberPicker thicknessNumberPicker = (NumberPicker) findViewById(R.id.thicknessNumberPicker);
-        thicknessNumberPicker.setDisplayedValues(SpeedCalculator.TIRE_THICKNESS_LIST);
-        thicknessNumberPicker.setMaxValue(SpeedCalculator.TIRE_THICKNESS_LIST.length - 1);
-        thicknessNumberPicker.setMinValue(0);
-        thicknessNumberPicker.setValue(preferences.getInt(TIRE_THICKNESS_INDEX_KEY, 0));
-
         final EditText wheelSizeEditText = (EditText) findViewById(R.id.wheelSizeEditText);
+
+        SharedPreferences preferences = getSharedPreferences(MainActivity.SHARED_PREFS_NAME, 0);
+        if (preferences.contains(MainActivity.WHEEL_SIZE_PREFS_KEY)) {
+            CharSequence sequence = String.valueOf(preferences.getInt(MainActivity.WHEEL_SIZE_PREFS_KEY, 0));
+            wheelSizeEditText.setText(sequence);
+        }
 
         Button saveButton = (Button) findViewById(R.id.saveButton);
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                float tireThickness = Float.valueOf(SpeedCalculator.TIRE_THICKNESS_LIST[thicknessNumberPicker.getValue()]);
 
-                int wheelSize;
-                if (wheelSizeEditText.getText().length() == 0) {
-                    wheelSize = SpeedCalculator.calculateWheelLength(diameterNumberPicker.getValue(), tireThickness);
-                } else {
-                    wheelSize = Integer.valueOf(wheelSizeEditText.getText().toString());
-                }
+                int wheelSize = Integer.valueOf(wheelSizeEditText.getText().toString());
 
                 SharedPreferences preferences = getSharedPreferences(MainActivity.SHARED_PREFS_NAME, 0);
                 SharedPreferences.Editor preferencesEditor = preferences.edit();
                 preferencesEditor.putInt(MainActivity.WHEEL_SIZE_PREFS_KEY, wheelSize);
-                preferencesEditor.putInt(RIM_DIAMETER_KEY, diameterNumberPicker.getValue());
-                preferencesEditor.putInt(TIRE_THICKNESS_INDEX_KEY, thicknessNumberPicker.getValue());
                 preferencesEditor.apply();
-
                 Intent intent = new Intent(v.getContext(), MainActivity.class);
                 startActivity(intent);
             }
--- android/app/src/main/java/org/catinthedark/smartbike/app/calculator/SpeedCalculator.java
+++ android/app/src/main/java/org/catinthedark/smartbike/app/calculator/SpeedCalculator.java
@@ -1,21 +1,16 @@
 package org.catinthedark.smartbike.app.calculator;
 
+import android.support.v4.util.ArrayMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by kirill on 06.07.14.
  */
 public class SpeedCalculator {
-    public static final int RIM_DIAMETER_MIN = 14;  // inches
-    public static final int RIM_DIAMETER_MAX = 30;
-
-    public static final String[] TIRE_THICKNESS_LIST = {
-            "1.9", "2.0", "2.1", "2.2", "2.3", "3.0", "4.0"
-    };
-
-    private static final float mmInInch = 25.6F;
-
-    public static int calculateWheelLength(int diameter, float thick) {
-        return (int) (Math.PI * ((diameter + thick * 2) * mmInInch));
-    }
+    private static final float KMH_IN_MPS = 3.6f;
+    private static final float MPH_IN_MPS = 2.2369f;
 
     public static float calculateSpeed(long lastTimestamp, long currentTimestamp, int wheelSize, boolean mph) {
         if (currentTimestamp < lastTimestamp) {
@@ -26,10 +21,10 @@ public class SpeedCalculator {
             speedInMps = (float) wheelSize / ((float) (currentTimestamp - lastTimestamp));
         }
         if (mph) {
-            return speedInMps * 10f;
+            return speedInMps * MPH_IN_MPS;
             //TODO: determine correct value of miles per hour
         } else {
-            return speedInMps * 3.6f;
+            return speedInMps * KMH_IN_MPS;
         }
     }
 }
